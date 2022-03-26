01a334a3678eea49239d41d63cf23820194e9077
Lilian Francis <ifnotlily@yahoo.com>
2022-03-26 19:58:22 +0100

    Updated migration files

--- db/migrate/20211211181533_add_posts_ref_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddPostsRefToComments < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :comments, :posts, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_182558) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_17_104833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "posts_id", null: false
-    t.bigint "users_id", null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
-    t.index ["users_id"], name: "index_comments_on_users_id"
+  create_table "Comment", force: :cascade do |t|
+    t.bigint "User_id", null: false
+    t.bigint "Post_id", null: false
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["Post_id"], name: "index_Comment_on_Post_id"
+    t.index ["User_id"], name: "index_Comment_on_User_id"
   end
 
-  create_table "likes", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id", unique: true
-    t.index ["user_id"], name: "index_likes_on_user_id", unique: true
+  create_table "Like", force: :cascade do |t|
+    t.bigint "User_id", null: false
+    t.bigint "Post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["Post_id"], name: "index_Like_on_Post_id"
+    t.index ["User_id"], name: "index_Like_on_User_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "string"
-    t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+  create_table "Post", force: :cascade do |t|
+    t.bigint "User_id", null: false
+    t.string "Title"
+    t.string "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "Comments_Counter", default: 0, null: false
+    t.integer "Likes_Counter", default: 0, null: false
+    t.index ["User_id"], name: "index_Post_on_User_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.integer "posts_counter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table "User", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.text "Bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "PostsCounter", default: 0, null: false
   end
 
-  add_foreign_key "comments", "posts", column: "posts_id"
-  add_foreign_key "comments", "users", column: "users_id"
-  add_foreign_key "posts", "users"
 end
